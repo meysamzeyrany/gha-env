c76d81a2f3117e7eb4b9e3583aa511930bb1792f
Meysam Zeyrani <rezvanclan@gmail.com>
2023-02-09 15:50:14 +0330

    db

--- data/database.js
+++ data/database.js
@@ -5,7 +5,9 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv:///${encodeURIComponent(dbUser)}:${encodeURIComponent(dbPassword)}@${clusterAddress}/?retryWrites=true&w=majority`;
+console.log({dbUser,dbPassword})
+
+const uri = `mongodb+srv:///${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
