7ba150585157506acc4171fe4726ae1ea4998215
Meysam Zeyrani <rezvanclan@gmail.com>
2023-02-09 15:40:41 +0330

    db

--- data/database.js
+++ data/database.js
@@ -5,20 +5,35 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv:///${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+  // const collection = client.db(dbName).collection('devices');
+  // perform actions on the collection object
+  // client.close();
+});
+
+// const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
-try {
-  await client.connect();
-  await client.db(dbName).command({ ping: 1 });
-  console.log('Connected successfully to server');
-} catch (error) {
-  console.log('Connection failed.');
-  await client.close();
-  console.log('Connection closed.');
-}
+// try {
+//   await client.connect();
+//   await client.db(dbName).command({ ping: 1 });
+//   console.log('Connected successfully to server');
+// } catch (error) {
+//   console.log('Connection failed.');
+//   await client.close();
+//   console.log('Connection closed.');
+// }
 
 const database = client.db(dbName);
 
